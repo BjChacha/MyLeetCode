221d64cca816fcd6938ff9584fab12a078b78c96
=BjChacha <bjchacha@outlook.com>
2022-06-07 08:23:56 +0800

    add: problem

---
+++ Problems/160. Intersection of Two Linked Lists/README.md
@@ -0,0 +1,12 @@
+---
+title: Intersection of Two Linked Lists
+no: 160
+difficulty: Easy
+tags: [Hash Table, Linked List, Two Pointers]
+url: https://leetcode.com/problems/intersection-of-two-linked-lists/
+---
+
+# 160. Intersection of Two Linked Lists
+
+[Leetcode](https://leetcode.com/problems/intersection-of-two-linked-lists/)
+
---
+++ Problems/160. Intersection of Two Linked Lists/Solution1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* node1 = headA;
+        ListNode* node2 = headB;
+        while(node1 && node2 && node1 != node2){
+            node1 = node1->next;
+            node2 = node2->next;
+            
+            if(node1 == node2) return node1;
+            
+            if(!node1) node1 = headB;
+            if(!node2) node2 = headA;
+        }
+        return node1;
+    }
+};
