104dd5645015509a5680dd60c860cfe9cc5e12a7
=BjChacha <bjchacha@outlook.com>
2022-07-30 12:28:37 +0800

    add: AcWing checkout

--- AcWing/Problems/3307. 破纪录者/Solution1.cpp
+++ AcWing/Problems/3307. 破纪录者/Solution1.cpp
@@ -16,7 +16,7 @@ int main() {
         int res;
         if (N == 1) res = 1;
         else {
-            int pre = -1, i = 0;
+            int pre = -1;
             res = 0;
             for (int i = 0; i < N; ++i) {
                 if ((i == 0 && a[i] > a[i+1] || i == N - 1 && a[i] > a[i-1] || a[i] > a[i-1] && a[i] > a[i+1]) && a[i] > pre) {
