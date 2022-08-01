5d84bfe93da1e1544624bcc94ceaabb3603cc8fb
=BjChacha <bjchacha@outlook.com>
2022-08-01 12:35:22 +0800

    fixed: recover try-catch

--- Tools/ProblemScraper/main.py
+++ Tools/ProblemScraper/main.py
@@ -42,19 +42,19 @@ def get_problem_info_LeetCode(url):
     return content
 
 def get_problem_info_AcWing(url):
-    # try:
-    page = requests.get(url)
-    soup = bs(page.text, 'lxml')
-    data = {}
+    try:
+        page = requests.get(url)
+        soup = bs(page.text, 'lxml')
+        data = {}
 
-    data['id'], data['title'] = soup.find('div', {'class': 'problem-content-title'}).text.strip().split('.')
-    data['title'] = data['title'].strip()
-    data['difficulty'] = soup.find('table', {'class': 'table-striped'}).tbody.td.span.text
-    data['tags'] = re.findall(r'keywords = \".*\"', soup.find('table', {'class': 'table-responsive'}).script.text)[0].split('=')[1].strip(' "').split(',')
+        data['id'], data['title'] = soup.find('div', {'class': 'problem-content-title'}).text.strip().split('.')
+        data['title'] = data['title'].strip()
+        data['difficulty'] = soup.find('table', {'class': 'table-striped'}).tbody.td.span.text
+        data['tags'] = re.findall(r'keywords = \".*\"', soup.find('table', {'class': 'table-responsive'}).script.text)[0].split('=')[1].strip(' "').split(',')
 
     return data
-    # except:
-    #     print('Get AcWing problem failed.')
+    except:
+       print('Get AcWing problem failed.')
 
 def convertLeetCodeData(data, flag):
     converted = {}
@@ -68,39 +68,42 @@ def convertLeetCodeData(data, flag):
     return converted
 
 def build_template(data):
-    problem_title = data['title']
-    # problem_title = problem_title.replace('?', '')
-    # if data['flag'] == 2:
-    #     problem_title = problem_title.split()[0]
-    problem_no = data['id']
-    problem_difficulty = data['difficulty']
-    problem_tags = f"[{', '.join(data['tags'])}]"
-    
-    source = 'LeetCode'
-    if data['flag'] == 3:
-        source = 'AcWing'
-    
-    folder_name = f"{DIRS[data['flag']]}/{problem_no}. {problem_title}"
-    file_name = '/'.join([folder_name, 'README.md'])
+    try:
+        problem_title = data['title']
+        # problem_title = problem_title.replace('?', '')
+        # if data['flag'] == 2:
+        #     problem_title = problem_title.split()[0]
+        problem_no = data['id']
+        problem_difficulty = data['difficulty']
+        problem_tags = f"[{', '.join(data['tags'])}]"
+        
+        source = 'LeetCode'
+        if data['flag'] == 3:
+            source = 'AcWing'
+        
+        folder_name = f"{DIRS[data['flag']]}/{problem_no}. {problem_title}"
+        file_name = '/'.join([folder_name, 'README.md'])
 
-    if os.path.isdir(folder_name):
-        print(f'Folder {folder_name} is existed.')
-    else:
-        os.makedirs(folder_name)
+        if os.path.isdir(folder_name):
+            print(f'Folder {folder_name} is existed.')
+        else:
+            os.makedirs(folder_name)
 
-    if os.path.isfile(file_name):
-        print(f'File {file_name} is existed.')
-    else:
-        content = FRONT_MATTER_TEMPLATE.format(
-            title=problem_title,
-            no=problem_no,
-            difficulty=problem_difficulty,
-            tags=problem_tags,
-            url=data['url'],
-            source=source,
-            )
-        with open(file_name, 'w', encoding='utf-8') as f:
-            f.writelines(content)
+        if os.path.isfile(file_name):
+            print(f'File {file_name} is existed.')
+        else:
+            content = FRONT_MATTER_TEMPLATE.format(
+                title=problem_title,
+                no=problem_no,
+                difficulty=problem_difficulty,
+                tags=problem_tags,
+                url=data['url'],
+                source=source,
+                )
+            with open(file_name, 'w', encoding='utf-8') as f:
+                f.writelines(content)
+    except:
+        print('Build template failed.')
 
 def url_preprocesser(url):
     if url.endswith('submissions/'):
